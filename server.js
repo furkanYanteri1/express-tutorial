726d3c5da26c284e789ffde33a347619e9fca9f9
Furkan Yanteri <furkanyanteri@gmail.com>
2024-05-21 16:43:47 +0300

    feat: middleware logger

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 
+// middleware -> go to the definition
+app.use(logger)
+app.use(logger2)
+
 app.get('/', (req, res) => {
     console.log('here')
     // res.status(500).json({ message: 'error'})
@@ -14,4 +18,12 @@ app.get('/', (req, res) => {
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
 
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+function logger2(req, res, next) {
+    console.log('again')
+    next()
+}
 app.listen(3000)
